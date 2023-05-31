1b4e08cba733a450b2f2077cb915b191ea2f8516
AzharShar <134915812+AzharShar@users.noreply.github.com>
2023-05-31 22:40:42 +0530

    Update tyres.py

--- tyres.py
+++ tyres.py
@@ -1,4 +1,20 @@
 import datetime from datetime
 
-def Carrigan_tyre:
-  list
+def Carrigan_Tyre:
+  list =[]
+  for i in range(3):
+    list[i]=float(input())
+    
+  for j in range(3):
+    if list[i]>=0.9:
+      carrigan_tyre_should_be_serviced()
+      
+ def Octoprime_Tyre:
+  list=[]
+  for i in range(3):
+     list[i]=float(input())
+    
+  for j in range(3):
+    if sum(list[i])>=3:
+     Octoprime_tyre_should_be_serviced()
+    
